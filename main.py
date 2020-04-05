bbd25886819ff412e1a3aa5ee436367289b26745
dell <przemo2048@gmail.com>
2020-04-05 17:23:47 +0200

    pd1 zad4

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Patient(BaseModel):
     name: str
     surename: str
 
+class PatientWithID(BaseModel):
+    id: int
+    patient: Patient
+
 app = FastAPI()
 app.counter = 0
 app.patients = []
@@ -31,16 +35,17 @@ def method_put():
 def method_delete():
     return {'method': 'DELETE'}
 
-@app.post('/patient')
+@app.post('/patient', response_model=PatientWithID)
 def patient_post(patient: Patient):
     app.patients.append(patient)
     app.counter += 1
-    return {"id": app.counter, "patient": patient}
+    return PatientWithID(id=app.counter, patient=patient)
+    # return {"id": app.counter, "patient": patient}
     # return {"id": app.counter, "patient": {"name": f'{patient.name}', "surename": f'{patient.surename}'}}
 
-@app.get('/patient/{pk}')
-def patient_get(pk):
-    id = int(pk)
+@app.get('/patient/{id}')
+def patient_get(id):
+    id = int(id)
     if id < 1 or id > app.counter:
         raise HTTPException(status_code=404, detail="Item not found")
     return app.patients[id-1]
