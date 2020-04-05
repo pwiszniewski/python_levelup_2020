47011e1a4848215a25010a23c5a4d2292b2dd0b1
dell <przemo2048@gmail.com>
2020-04-05 16:02:08 +0200

    pd1 zad3

--- main.py
+++ main.py
@@ -4,20 +4,8 @@ app = FastAPI()
 
 @app.get('/')
 def hello_world():
-    return {'message': 'Hello World during the coronavirus pandemic!'}
+    return {'message': 'Hello world'}
 
-@app.get('/method')
-def hello_world():
-    return {'method': 'GET'}
-
-@app.post('/method')
-def hello_world():
-    return {'method': 'POST'}
-
-@app.put('/method')
-def hello_world():
-    return {'method': 'PUT'}
-
-@app.delete('/method')
-def hello_world():
-    return {'method': 'DELETE'}
+@app.get('/hello/{name}')
+def hello_name(name: str):
+    return {'message': f'Hello {name}'}
