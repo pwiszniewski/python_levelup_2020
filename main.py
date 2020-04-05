413301e16ac22142d73e23270ed27485afd7902d
dell <przemo2048@gmail.com>
2020-04-05 16:04:45 +0200

    pd1 zad3

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
+class Patient(BaseModel):
+    name: str
+    surename: str
+
 app = FastAPI()
+app.counter = -1
 
 @app.get('/')
 def hello_world():
-    return {'message': 'Hello world'}
+    return {'message': 'Hello World during the coronavirus pandemic!'}
 
-@app.get('/hello/{name}')
-def hello_name(name: str):
-    return {'message': f'Hello {name}'}
+@app.get('/method')
+def method_get():
+    return {'method': 'GET'}
+
+@app.post('/method')
+def method_post():
+    return {'method': 'POST'}
+
+@app.put('/method')
+def method_put():
+    return {'method': 'PUT'}
+
+@app.delete('/method')
+def method_delete():
+    return {'method': 'DELETE'}
+
+@app.post('/patient')
+def patient_post(patient: Patient):
+    app.counter += 1
+    return {"id": app.counter, "patient": {"name": f'{patient.name}', "surename": f'{patient.surename}'}}
+
+
