0347f97d236670cc4600af3b917a5bef01120da2
dell <przemo2048@gmail.com>
2020-04-26 10:54:14 +0200

    PD3 zad1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 
 from pydantic import BaseModel
 
+app = FastAPI()
+
 @app.get('/')
 def hello_world():
     return {'message': 'Hello World during the coronavirus pandemic!'}
