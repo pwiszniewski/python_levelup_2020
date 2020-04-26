80fc324be5e749f2fe1436cc1c2264ace4d350c5
dell <przemo2048@gmail.com>
2020-04-26 13:10:00 +0200

    PD3 zad4

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import secrets
 from hashlib import sha256
 
-from fastapi import Depends, FastAPI, HTTPException, status, Cookie, Response
+from fastapi import Depends, FastAPI, HTTPException, status, Cookie, Response, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
 app.secret_key = "99d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8aa34"
 app.sessions = {}
+
+templates = Jinja2Templates(directory="templates")
 
 security = HTTPBasic()
 
@@ -28,13 +32,13 @@ def get_session_token(credentials: HTTPBasicCredentials = Depends(security)):
     session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
     if session_token not in app.sessions:
         app.sessions[session_token] = credentials.username
-        response.set_cookie(key="session_token", value=session_token)
     return session_token
 
 
 @app.post("/login")
 def login_user(session_token: str = Depends(get_session_token)):
     response = RedirectResponse(url='/welcome')
+    response.set_cookie(key="session_token", value=session_token)
     return response
 
 @app.post("/logout")
@@ -51,4 +55,3 @@ def welcome(*, request: Request, response: Response, session_token: str = Cookie
     if session_token not in app.sessions:
         raise HTTPException(status_code=403, detail="Unathorised")
     return templates.TemplateResponse("welcome.html", {"request": request, "user": app.sessions[session_token]})
-    
